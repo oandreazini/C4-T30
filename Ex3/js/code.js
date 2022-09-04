e10933a60a27e132b29697ad1ef1e37e7d0e965a
oandreazini <oriol_as_92@hotmail.com>
2022-09-04 11:46:36 +0200

    Ex3 finished

---
+++ Ex3/js/code.js
@@ -0,0 +1,8 @@
+function allSelect() {
+    document.getElementById("box").select = alert("Se presiono un párrafo del documento.");
+
+}
+
+function secondTable() {
+    document.getElementById("table2").select = alert("Se presiono un párrafo contenido en la segunda tabla.");
+}
